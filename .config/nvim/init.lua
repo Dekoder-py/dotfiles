8763c405aff6f30698a273d8b8dc00ad3013a232
Dekoder-py <kyleyesmariam@icloud.com>
2025-02-18 20:30:09 +1300

    add lazygit.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,6 +26,27 @@ require('lazy').setup({
   }
 },
 { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
+{
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+}
+
 })
 
 -- [[ Set colorscheme to catppuccin ]]
